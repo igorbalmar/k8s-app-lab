aa91527ddfe6c5fd9845f01173ccaa893bc53d2f
Igor Balsanelli <igor.balsanelli@gmail.com>
2025-08-08 08:49:06 -0300

    ajuste de requests argo

--- 00-iac/terraform/module/argo.tf
+++ 00-iac/terraform/module/argo.tf
@@ -1,15 +1,57 @@
 resource "helm_release" "argocd" {
-  chart             = "argo-cd"
-  name              = "argocd"
-  namespace         = "argocd"
-  repository        = "https://argoproj.github.io/argo-helm"
-  version           = var.argoVersion
-  create_namespace  = true
+  chart            = "argo-cd"
+  name             = "argocd"
+  namespace        = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  version          = var.argoVersion
+  create_namespace = true
 
-  depends_on        = [
+  values = [
+    yamlencode({
+      controller = {
+        resources = {
+          requests = {
+            cpu    = "250m"
+            memory = "512Mi"
+          }
+          limits = {
+            cpu    = "500m"
+            memory = "1Gi"
+          }
+        }
+      }
+      repoServer = {
+        resources = {
+          requests = {
+            cpu    = "250m"
+            memory = "256Mi"
+          }
+          limits = {
+            cpu    = "500m"
+            memory = "512Mi"
+          }
+        }
+      }
+      server = {
+        resources = {
+          requests = {
+            cpu    = "250m"
+            memory = "256Mi"
+          }
+          limits = {
+            cpu    = "500m"
+            memory = "512Mi"
+          }
+        }
+      }
+    })
+  ]
+
+  depends_on = [
     azurerm_kubernetes_cluster.aks_lab
-    ]
+  ]
 }
+
 
 resource "helm_release" "argo_rollouts" {
   name              = "argo-rollouts"
